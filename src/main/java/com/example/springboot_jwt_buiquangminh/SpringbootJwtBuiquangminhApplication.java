513fecfc452688c054f9c5618fa5d3e57c539cce
LAPTOP-S6AMOK8K\meobi <buiquangminh2501@gmail.com>
2021-09-23 22:40:49 +0700

    commit entity

---
+++ src/main/java/com/example/springboot_jwt_buiquangminh/SpringbootJwtBuiquangminhApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_buiquangminh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtBuiquangminhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtBuiquangminhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_jwt_buiquangminh/SpringbootJwtBuiquangminhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jwt_buiquangminh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtBuiquangminhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
